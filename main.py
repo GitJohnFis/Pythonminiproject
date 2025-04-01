7a661d771bc082cf64427d6607cd154b2a390dfc
GitJohnFis <jfisiiahi69@gmail.com>
2025-04-02 10:34:04 +1300

    Include harsh element variables that can affect Twiggy in main.py
    Introduce heat and wind variables that values are based on the values of other variables .

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ name = "Twiggy"
 leaves = 0
 light = 1
 cover = 1
+wind = 10
+wind = wind - (5 * cover)
 water = 1
 for day in range(3)
 leaves = leaves + 1
 leaves = leaves + light
 leaves = leaves + water
 light = light - cover
+heat = light * 10
+print("Light:", light)
+print("Heat:", heat)
 if leaves > 8:
  print("Growth spurt!")
 if leaves == 3:
